41a26d14d53d790eb9fa92e2531a4e4388138cb3
João Vitor Alves Amaral <am.joao13@gmail.com>
2025-06-04 12:39:23 -0300

    Página Finalizada

---
+++ README.md
@@ -0,0 +1,9 @@
+## Instruções ##
+
+1) Para iniciar, abra o terminal e digite npm start.
+2) Para utilizar o favoritos e cadastro, você deve fazer login.
+3) Crie seu usuário e senha no login.
+4) Seu usuário não será admin, então você só pode acessar os favoritos.
+5) Para adicionar os privilégios de admin, vá em db.json, no seu usuário, altere admin para true.
+6) Você deve fechar e abrir a página para que seja alterado.
+7) Agora você pode cadastrar e favoritar.
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,12 @@
 /* JS para CRUD */
-const apiUrl = '/noticias';
+const apiUrl1 = '/noticias';
+
+const LOGIN_URL = "login.html";
+
+const apiUrl2 = '/usuarios';
 
 function readNoticia(processaDados) {
-    fetch(apiUrl)
+    fetch(apiUrl1)
         .then(response => response.json())
         .then(data => {
             processaDados(data);
@@ -14,7 +18,7 @@ function readNoticia(processaDados) {
 }
 
 function createNoticia(noticia, refreshFunction) {
-    fetch(apiUrl, {
+    fetch(apiUrl1, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -38,7 +42,7 @@ function createNoticia(noticia, refreshFunction) {
 }
 
 function updateNoticia(id, noticia, refreshFunction) {
-    fetch(`${apiUrl}/${id}`, {
+    fetch(`${apiUrl1}/${id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -56,7 +60,7 @@ function updateNoticia(id, noticia, refreshFunction) {
 }
 
 function deleteNoticia(id, refreshFunction) {
-    fetch(`${apiUrl}/${id}`, {
+    fetch(`${apiUrl1}/${id}`, {
         method: 'DELETE',
     })
         .then(response => {
@@ -75,6 +79,12 @@ function displayMessage(msg) {
 
 /* Favoritos */
 function favorito(elemento, id) {
+    const usuarioCorrente = JSON.parse(sessionStorage.getItem("usuarioCorrente"));
+    if (!usuarioCorrente || !usuarioCorrente.login) {
+        alert("Você precisa estar logado para favoritar uma notícia.");
+        return;
+    }
+
     fetch("http://localhost:3000/noticias")
         .then(res => res.json())
         .then(data => {
@@ -88,6 +98,7 @@ function favorito(elemento, id) {
                 elemento.src = "/assets/img/coracao.png";
                 confereFavorito = false;
             }
+
             fetch(`http://localhost:3000/noticias/${id}`, {
                 method: "PATCH",
                 headers: {
@@ -97,45 +108,156 @@ function favorito(elemento, id) {
                     favorito: confereFavorito
                 })
             })
+
         })
 }
 
-/* Pesquisa */
-const searchInput = document.getElementById("search");
+/* Esconde Favoritos e Cadastro */
 
-searchInput.addEventListener("input", (event) => {
+function confereUsuario() {
+    const usuarioCorrente = JSON.parse(sessionStorage.getItem('usuarioCorrente'));
+    const cadastroLink = document.getElementById('cadastroHeader');
+    const favoritoLink = document.getElementById('favoritoHeader');
+    const deslogar = document.getElementById('loginHeader');
 
-    const value = formatString(event.target.value);
+    cadastroLink.style.display = 'none';
+    favoritoLink.style.display = 'none';
 
-    const items = document.querySelectorAll(".cardNoticia");
+    if (usuarioCorrente && usuarioCorrente.login) {
+        deslogar.innerHTML = "<u>Logout</u>";
+        deslogar.href = "#";
+        deslogar.addEventListener("click", function (e) {
+            e.preventDefault();
+            logoutUser();
+        });
 
-    const noResults = document.getElementById("pesquisaNenhum");
+        favoritoLink.style.display = 'inline';
 
-    let hasResults = false;
-
-    items.forEach(cardNoticia => {
-        cardText = formatString(cardNoticia.textContent)
-        if (cardText.indexOf(value) !== -1) {
-            cardNoticia.style.display = "flex";
-            hasResults = true;
+        if (usuarioCorrente.admin == true) {
+            cadastroLink.style.display = 'inline';
         }
-        else {
-            cardNoticia.style.display = "none";
-        }
-    })
-    if (hasResults)
-        noResults.style.display = "none";
-    else
-    noResults.style.display = "block"
-});
+    } else {
+        deslogar.innerHTML = "<u>Login</u>";
+        deslogar.href = "login.html";
+    }
+};
 
-function formatString(value) {
-    return value.toLowerCase()
-        .trim()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
+function logoutUser() {
+    fetch("http://localhost:3000/noticias")
+        .then(res => res.json())
+        .then(data => {
+            const favoritosMarcados = data.filter(noticia => noticia.favorito === true);
+
+            favoritosMarcados.forEach(noticia => {
+                fetch(`http://localhost:3000/noticias/${noticia.id}`, {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ favorito: false })
+                });
+            });
+
+            sessionStorage.setItem('usuarioCorrente', JSON.stringify({}));
+            window.location = LOGIN_URL;
+        });
 }
 
+/* JS Para Login/User */
+
+var db_usuarios = {};
+
+var usuarioCorrente = {};
+
+function generateUUID() {
+    var d = new Date().getTime();
+    var d2 = (performance && performance.now && (performance.now() * 1000)) || 0;
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16;
+        if (d > 0) {
+            r = (d + r) % 16 | 0;
+            d = Math.floor(d / 16);
+        } else {
+            r = (d2 + r) % 16 | 0;
+            d2 = Math.floor(d2 / 16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+const dadosIniciais = {
+    usuarios: [
+        { "id": generateUUID(), "login": "admin", "senha": "123", "nome": "Administrador do Sistema", "email": "admin@abc.com", "admin": false },
+        { "id": generateUUID(), "login": "user", "senha": "123", "nome": "Usuario Comum", "email": "user@abc.com", "admin": false },
+    ]
+};
+
+function initLoginApp() {
+    usuarioCorrenteJSON = sessionStorage.getItem('usuarioCorrente');
+    if (usuarioCorrenteJSON) {
+        usuarioCorrente = JSON.parse(usuarioCorrenteJSON);
+    }
 
 
-/* Login */
+    fetch(apiUrl2)
+        .then(response => response.json())
+        .then(data => {
+            db_usuarios = data;
+        })
+        .catch(error => {
+            console.error('Erro ao ler usuários via API JSONServer:', error);
+            displayMessage("Erro ao ler usuários");
+        });
+};
+
+function loginUser(login, senha) {
+    return fetch(apiUrl2)
+        .then(response => response.json())
+        .then(data => {
+            for (let usuario of data) {
+                if (login === usuario.login && senha === usuario.senha) {
+                    usuarioCorrente = {
+                        id: usuario.id,
+                        login: usuario.login,
+                        email: usuario.email,
+                        nome: usuario.nome,
+                        admin: usuario.admin
+                    };
+
+                    sessionStorage.setItem('usuarioCorrente', JSON.stringify(usuarioCorrente));
+                    return true;
+                }
+            }
+            return false;
+        })
+        .catch(error => {
+            console.error("Erro ao fazer login:", error);
+            return false;
+        });
+}
+
+function addUser(nome, login, senha, email, admin) {
+
+    let newId = generateUUID();
+    let usuario = { "id": newId, "login": login, "senha": senha, "nome": nome, "email": email, "admin": false };
+
+    fetch(apiUrl2, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(usuario),
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            db_usuarios.push(usuario);
+            displayMessage("Usuário inserido com sucesso");
+        })
+        .catch(error => {
+            console.error('Erro ao inserir usuário via API JSONServer:', error);
+            displayMessage("Erro ao inserir usuário");
+        });
+}
+
+initLoginApp();
